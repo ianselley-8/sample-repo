d7501794e827b3cc28d0bb9f23821d29ef5daeaf
ianselley-8 <ianprogra8@gmail.com>
2020-06-12 11:45:07 +0200

    implement bs4

--- Proj_de_tensor/test.py
+++ Proj_de_tensor/test.py
@@ -6,3 +6,8 @@ print(tf.__version__)
 print(keras.__version__)
 r=requests.get('https://coreyms.com')
 print(r.ok)
+
+real=constant([1, 5])
+imag=constant([2, 4])
+cmpx=tf.complex(real, imag)
+print(cmpx.args)
